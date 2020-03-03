65c878cc623be0c915c2733fe0ebc652ed66fa1a
Peter <p.dyumin@dunice.net>
2020-03-03 12:01:14 +0300

    Initial

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import * as Koa from 'koa';
+import * as Router from 'koa-router';
+
+import * as bodyParser from 'koa-bodyparser';
+import * as json from 'koa-json';
+import * as logger from 'koa-logger';
+
+const app = new Koa();
+const router = new Router();
+
+router.get('/', (ctx: Koa.Context): void => {
+  ctx.status = 200;
+  ctx.body = {
+    datetime: Date.now(),
+    info: 'OK',
+    status: ctx.status,
+  };
+  return ctx.body;
+});
+
+app.use(bodyParser());
+app.use(json());
+app.use(logger());
+
+// enable routing
+app.use(router.routes());
+
+// launch the server
+const PORT = Number(process.env.PORT) || 2020;
+/* tslint:disable-next-line:no-console */
+app.listen(PORT, () => console.log(`-- Server is running on port ${PORT}`));
